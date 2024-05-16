b5263dd67a4802c8b1612e88d5679324811bcda3
Lucas Vinicius <howaitosame13@gmail.com>
2024-05-15 21:08:58 -0300

    iteração e propriedades do post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+/* eslint-disable react/jsx-key */
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from './App.module.css';
 import { Post } from './components/Post.jsx';
 import { Sidebar } from "./components/Sidebar.jsx";
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://i.pinimg.com/736x/77/c4/3f/77c43f4548cc2ea76d0606fa77eaf45f.jpg',
+      name: 'Chung Ha',
+      role: 'Java Developer @ I.O.I'
+    },
+    content: [
+      { type: 'paragraph', content: 'Lorem ipsum'},
+      { type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, perferee' },
+      { type: 'link', content: 'lkvs.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://love4everkm.files.wordpress.com/2021/01/yeji-1-1.jpg',
+      name: 'Yeji',
+      role: 'Software Engineer @ Itzy'
+    },
+    content: [
+      { type: 'paragraph', content: 'Lorem ipsum'},
+      { type: 'paragraph', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, perferee' },
+      { type: 'link', content: 'lkvs.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00'),
+  }
+];
+
 
 export function App() {
 
@@ -13,14 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post 
-            author="Lucas vinicius"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ducimus provident id quo distinctio repellendus aliquid sequi nemo eum libero commodi aut mollitia porro totam aspernatur, quidem, quibusdam voluptates. Atque."
-          />
-          <Post 
-            author="Lucas vinicius"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ducimus provident id quo distinctio repellendus aliquid sequi nemo eum libero commodi aut mollitia porro totam aspernatur, quidem, quibusdam voluptates. Atque."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,46 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import styles from './Post.module.css';
-import ProfileImg from '../assets/goddesss.png'
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
-export function Post(){
+export function Post({author, publishedAt, content}){
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={ProfileImg} />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Lucas Vinicius</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="11 de Maio ás 08:13h" dateTime="2024/05/12 08:13:00">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
 
             </header>
 
             <div className={styles.content}>
-                <p>Lorem ipsum</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, perferee</p>
-                <p>
-                    <a href="">#Test</a>{' '}
-                    <a href="">#NewProject</a>{' '}
-                    <a href="">#Rocketseat</a>
-                </p>
+                {content.map(line =>{
+                    if (line.type === 'paragraph'){
+                        return <p>{line.content}</p>;
+                    }else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>;
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
